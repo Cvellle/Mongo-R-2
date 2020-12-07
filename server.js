8a8e514d1bc2626571a1439567f5dc44a3b2f70d
cvellle <cuekisa@yahoo.com>
2020-12-07 01:20:40 +0100

    MongoDB Atlas new enpoint

--- server.js
+++ server.js
@@ -4,24 +4,21 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Add routes, both API and view
-require('./routes/api-routes')(app);
+require("./routes/api-routes")(app);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://Cvele:cveledb1@ds129914.mlab.com:29914/posts",
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb+srv://cvele:cvelePass@posts.jzao1.mongodb.net/test",
   { useNewUrlParser: true }
 );
 
-// Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`??  ==> API Server now listening on PORT ${PORT}!`);
 });
